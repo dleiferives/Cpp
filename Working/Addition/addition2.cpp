0f22bdfe9e869a053974bdea6a4c9cf18721685e
Dylan Leifer-Ives <40322629+dleiferives@users.noreply.github.com>
2020-07-25 21:43:24 -0700

    I wrote another version and its a little different.

---
+++ Working/Addition/addition2.cpp
@@ -0,0 +1,19 @@
+// By Dylan Leifer-Ives 7-25-2020: 21:13 A different way to write an adition script. Wanna see if it works
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+	int x;
+	int y;
+	int z;
+
+		
+	std::cin >> x;
+	std::cin >> y;
+	
+	z = x+y;
+	std::cout << z;
+//finished 21:16
+//21:17-8 or not forgot to declare z and make print work with an escape character.
+// For sure was not done, just learned some stuff and decied to go with cout like usual 21:28
+}
