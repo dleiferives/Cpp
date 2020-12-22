e530e1ca6649674ccd49671368396745b2a5ed91
dleiferives <dleiferives@gmail.com>
2020-12-21 22:17:27 -0800

    Added some more shit

--- Testing/Perspective/y_eq_mx_plus_b.cpp
+++ Testing/Perspective/y_eq_mx_plus_b.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+//used structures
 struct point
 {
 	float x;
@@ -13,8 +15,30 @@ struct vp_line
 	point p2; // coordinates of point 2 in the line
 	point intercept; //x and y intercept
 };
-	
-void find_m(vp_line & line)
+
+
+// global variables
+	point vp_3;
+	point vp_2;
+	point vp_1;
+	int point_iterator =1;
+
+
+
+//templates
+ 	
+
+
+//functions
+void fill_point(point & i_point)
+{
+	printf("Point #%i x : ",point_iterator);
+	scanf("%f", &i_point.x);
+	printf("Point #%i y : ",point_iterator);
+	scanf("%f", &i_point.y);
+	point_iterator++;
+}
+void find_m(vp_line & line) // just some simple arithmetic to find the slope of the line
 {
 	float rise = line.p2.y - line.p1.y; // the height minus the hight aka the rise
 	//testing if the line is vertical
@@ -28,61 +52,81 @@ void find_m(vp_line & line)
 		float run = line.p2.x - line.p1.x;
 		line.m = rise/run;
 	}
-	
 }
 
-void find_y_intercept(vp_line & line)
-{
+void find_y_intercept(vp_line & line) // a function that finds the y intercept of a given line
+{				      // stores the value into the intercept value
 	line.intercept.y = (line.p1.y)-((line.m)*(line.p1.x));
 
 }
 
+void find_vp(point &vp, vp_line line1, vp_line line2)// finds the intersection point of two given lines, aka my guy the vanishing point
+{
+	if( line1.m != line2.m) 
+	{
+		vp.x = ((line2.intercept.y-line1.intercept.y)/(line1.m-line2.m));
+	        vp.y = (line1.m * vp.x) + line1.intercept.y;	
+	}
+	else
+	{
+		printf("lines are parrelel no vanishing point\n");
+	}
+}
+
+void fill_vp_line(vp_line & line, point vp1, point vp2)
+{
+	line.p1 = vp1;
+	line.p2 = vp2;
+	find_m(line);
+	find_y_intercept(line);
+}
+
 int main()
 {
-	point p1;
 	printf("Cheks if three points lie on the same line.\n");
-	printf("Point #1 x : ");
-	scanf("%f", &p1.x);
-	printf("Point #1 y : ");
-	scanf("%f", &p1.y);
-	
-	point p2;
-	printf("Point #2 x : ");
-	scanf("%f", &p2.x);
-	printf("Point #2 y : ");
-	scanf("%f", &p2.y);
+	point p1, p2, p3, p4;
+	fill_point(p1);
+	fill_point(p2);
+	fill_point(p3);
+	fill_point(p4);
 
-	point p3;
-	printf("Point #3 x : ");
-	scanf("%f", &p3.x);
-	printf("Point #3 y : ");
-	scanf("%f", &p3.y);
+
 	
 	//creating the line that we will be checking.
 	
-	vp_line test_line;
-	test_line.p1 = p1;
-	test_line.p2 = p2;
+	vp_line vp1_line1, vp1_line2, vp2_line1, vp2_line2;
+	fill_vp_line(vp1_line1, p1, p2);
+	fill_vp_line(vp1_line2, p3, p4);	
+	fill_vp_line(vp2_line1, p1, p3);
+	fill_vp_line(vp2_line2, p2, p4);
+	
+	printf(" m = %f ", vp1_line1.m);
+	printf(" b = %f \n",vp1_line1.intercept.y);
 
-	find_m(test_line); // finds the slope of the line rise over run
-	find_y_intercept(test_line);
-	printf(" m = %f", test_line.m);
-	printf(" b = %f ",test_line.intercept.y);
+	printf(" m = %f ", vp1_line2.m);
+	printf(" b = %f \n",vp1_line2.intercept.y);
+
+	printf(" m = %f ", vp2_line1.m);
+	printf(" b = %f \n",vp2_line1.intercept.y);
 	
-	float h_x3 = ((test_line.m * p3.x) + test_line.intercept.y);
-	printf("line at x3 %f", h_x3);
+	printf(" m = %f ", vp2_line2.m);
+	printf(" b = %f \n",vp2_line2.intercept.y);
+
+	float h_x3 = ((vp1_line1.m * p3.x) + vp1_line1.intercept.y);
+	printf("line at x3 %f\n", h_x3); // the height where the line given by p1 and p2 crosses point 3's x coordinate
 	//testing if point 3 is within 10%
-	
-	
-	//float tmp1 = (test_line.p3.y * 0.9);
-	//float tmp2 = (test_line.p3.y * 1.1);  
-	
-	//printf("\n tmp1 %f tmp2 %f ",tmp1,tmp2);	
 
 	if( h_x3 >= (p3.y * 0.9) && h_x3 <= (p3.y * 1.10))
 	{
-		printf(" you hit it big boy");
+		printf(" you hit it big boy\n");
 	}
+	
+	find_vp(vp_1, vp1_line1, vp1_line2);
+	find_vp(vp_2, vp2_line1, vp2_line2);
+
+	printf(" Vanishing Point(%f,%f) \n",vp_2.x, vp_2.y);
+	printf(" Vanishing Point(%f,%f) \n",vp_1.x, vp_1.y);
 
 	return 0;
 }
+	
